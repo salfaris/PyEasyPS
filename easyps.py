faa32d72f09e23b7fcf3007a58c673ebe983c725
Salman <salmanfaris98@gmail.com>
2021-06-07 19:40:50 +0100

    UPDATE: Introduced UniPSObject

--- easyps.py
+++ easyps.py
@@ -1,15 +1,15 @@
 import os
 
-# from pylatex.base_classes import CommandBase
 from pylatex import Document, Command, Package
 from pylatex.utils import NoEscape
 
 class EasyPS(Document):
-    def __init__(self):
+    def __init__(self, student_name=None):
         super().__init__()
         self._title = None
-        self._student_name = None
-        self._uni_ps = {}
+        self._student_name = student_name
+        self._uni_ps_dict = {}
+        self._content_dir = './content'
 
         # Document class settings
         self.documentclass = Command(
@@ -29,15 +29,41 @@ class EasyPS(Document):
             arguments='1.'))
         self.change_length(r'\parskip', '0.5em')
 
+    def add_ps(self, uni_objs):
+        for uni_obj in uni_objs:
+            tex_name = os.path.splitext(uni_obj.tex_filename)[0]
+            self._uni_ps_dict[tex_name] = uni_obj
+
+    def make_ps(self, for_tex_filename, make_tex=True):
+        tex_name = os.path.splitext(for_tex_filename)[0]
+        try:
+            uni_obj = self._uni_ps_dict[tex_name]
+        except KeyError as err:
+            raise err
+        
+        self._make_title(uni_obj.course_name, uni_obj.show_title)
+        
+        tex_filename = uni_obj.tex_filename
+        path_to_tex = f'{self._content_dir}/{tex_filename}'
+        print(path_to_tex)
+        if tex_filename not in os.listdir(self._content_dir):
+            err_msg = (f"Cannot find TeX file '{tex_filename}' in " 
+                       + f"content directory '{self._content_dir}'")
+            raise FileNotFoundError(err_msg)
+        
+        self.append(NoEscape(r'\input{%s}' % path_to_tex))
+        
+        self.generate_pdf('main', clean_tex=(not make_tex))
+
     def _make_title(self, course_name, is_show_title):
         if self._student_name is None:
-            raise NameError("Expect student name to be not None.")
+            raise EasyPSNameError("Expect student name to be not None.")
         if not is_show_title:
             return
 
         title_text = (
-            rf'{self._student_name} ---'
-            + rf'Personal Statement for {course_name}')
+            f'{self._student_name} ---'
+            + f' Personal Statement for {course_name}')
 
         title_display_text = (
             r"""
@@ -49,31 +75,7 @@ class EasyPS(Document):
             \end{center}
             """ % title_text)
         self.append(NoEscape(title_display_text))
-
-
-    def add_ps(self, *uni_ps):
-        for ps in uni_ps:
-            tex_name = os.path.splitext(ps['tex_filename'])[0]
-            self._uni_ps[tex_name] = ps
-
-    def make_ps(self, for_tex_filename, make_tex=True):
-        try:
-            tex_name = os.path.splitext(for_tex_filename)[0]
-            ps = self._uni_ps[tex_name]
-        except PsNotAddedError:
-            raise
         
-        self._make_title(ps['course_name'], ps['show_title'])
-        
-        tex_filename = ps['tex_filename']
-        path_to_tex = f'content/{tex_filename}'
-        
-        # NOTE :- Add os.path to crawl for filenames and
-        #       terminate before appending to docs to prevent
-        #       pdflatex error.
-        self.append(NoEscape(r'\input{%s}' % path_to_tex))
-        self.generate_pdf('main', clean_tex=(not make_tex))
-
     @property
     def student_name(self):
         return self._student_name
@@ -81,11 +83,31 @@ class EasyPS(Document):
     @student_name.setter
     def student_name(self, new_name):
         self._student_name = new_name
+        
 
-class NameError(Exception):
+class UniPSObject:
+    def __init__(self, tex_filename=None, course_name=None, show_title=None):
+        self._tex_filename = tex_filename
+        self._course_name = course_name
+        self._show_title = show_title
+        
+    @property
+    def tex_filename(self):
+        return self._tex_filename
+    
+    @property
+    def course_name(self):
+        return self._course_name
+    
+    @property
+    def show_title(self):
+        return self._show_title
+
+
+class EasyPSNameError(Exception):
     pass
 
 class PsNotAddedError(KeyError):
-    def __init__(self):
-        self.message = "Ps was not added."
+    def __init__(self, message):
+        self.message = message
         super().__init__(self.message)
